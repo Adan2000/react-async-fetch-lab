14e2c9ac2212eb516817d0ac6d65d9bd938f32b1
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-21 18:51:58 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,11 @@ export default class App extends Component {
             })
         })
     }
-    
+    //here we use the componentDidMount for a fetch GET request.
+    //we turn that returned data in JSON
+    //we then use the setState to change the initial state, to the data from 
+    //the requested api data
+
     render() {
         return (
             <div>
@@ -26,3 +30,7 @@ export default class App extends Component {
         )
     }
 }
+//here we use this.state.peopleInSpace.map to create a new array 
+//with the results of calling a function or event for every element.
+//we use person and id as the two argumenets, we set the id as the key of the person
+//and we use the person to display the name of each one.
